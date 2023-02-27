fa9541cf3675f66cffff3eba7e803fc00493ac62
milan <milanjulinac996@gmail.com>
2023-02-27 10:48:52 +0100

    added routes

--- src/routes/basketType.route.ts
+++ src/routes/basketType.route.ts
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import {
-  createBasketType,
-  getBasketTypes,
-  getBasketTypeById,
-  updateBasketTypeById,
-  deleteBasketTypeById,
-  softDeleteBasketTypeById
+	createBasketType,
+	getBasketTypes,
+	getBasketTypeById,
+	updateBasketTypeById,
+	deleteBasketTypeById,
+	softDeleteBasketTypeById
 } from "../conrollers/basketType.controller";
+import authMiddleware from "../middleware/JWT";
 
 const router: Router = Router();
 
 // CREATE a new basket type
-router.post("/create", createBasketType);
+router.post("/create", authMiddleware, createBasketType);
 
 // READ all basket types
 router.get("/find-all", getBasketTypes);
@@ -20,12 +21,12 @@ router.get("/find-all", getBasketTypes);
 router.get("/find/:id", getBasketTypeById);
 
 // UPDATE a basket type by ID
-router.patch("/update/:id", updateBasketTypeById);
+router.patch("/update/:id", authMiddleware, updateBasketTypeById);
 
 // DELETE a basket type by ID
-router.delete("/delete/:id", deleteBasketTypeById);
+router.delete("/delete/:id", authMiddleware, deleteBasketTypeById);
 
 // SOFT DELETE a basket type by ID
-router.delete("/remove/:id", softDeleteBasketTypeById)
+router.delete("/remove/:id", authMiddleware, softDeleteBasketTypeById);
 
 export default router;
