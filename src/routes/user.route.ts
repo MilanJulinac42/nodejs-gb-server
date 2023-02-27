b7d1af91a2ebb783d178025095f9dd9137b19902
milan <milan@millions.co>
2023-02-27 10:48:52 +0100

    added routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,22 +6,23 @@ import {
 	deleteUserById,
 	softDeleteUserById
 } from "../conrollers/user.controller";
+import authMiddleware from "../middleware/JWT";
 
 const router: Router = Router();
 
 // READ all users
-router.get("/find-all", getUsers);
+router.get("/find-all", authMiddleware, getUsers);
 
 // READ a single user by ID
-router.get("/find/:id", getUserById);
+router.get("/find/:id", authMiddleware, getUserById);
 
 // UPDATE a user by ID
-router.patch("/update/:id", updateUserById);
+router.patch("/update/:id", authMiddleware, updateUserById);
 
 // DELETE a user by ID
-router.delete("/delete/:id", deleteUserById);
+router.delete("/delete/:id", authMiddleware, deleteUserById);
 
 // SOFT DELETE a user by ID
-router.delete("/remove/:id", softDeleteUserById);
+router.delete("/remove/:id", authMiddleware, softDeleteUserById);
 
 export default router;
