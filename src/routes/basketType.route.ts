0b3aafc646d1006589d1274599ecdb5c7f2d4ae9
milan <milan@millions.co>
2023-02-27 11:26:54 +0100

    added authorization for admin routes

--- src/routes/basketType.route.ts
+++ src/routes/basketType.route.ts
@@ -7,12 +7,13 @@ import {
 	deleteBasketTypeById,
 	softDeleteBasketTypeById
 } from "../conrollers/basketType.controller";
+import { isAdmin } from "../middleware/IsAdmin";
 import authMiddleware from "../middleware/JWT";
 
 const router: Router = Router();
 
 // CREATE a new basket type
-router.post("/create", authMiddleware, createBasketType);
+router.post("/create", authMiddleware, isAdmin, createBasketType);
 
 // READ all basket types
 router.get("/find-all", getBasketTypes);
@@ -21,12 +22,12 @@ router.get("/find-all", getBasketTypes);
 router.get("/find/:id", getBasketTypeById);
 
 // UPDATE a basket type by ID
-router.patch("/update/:id", authMiddleware, updateBasketTypeById);
+router.patch("/update/:id", authMiddleware, isAdmin, updateBasketTypeById);
 
 // DELETE a basket type by ID
-router.delete("/delete/:id", authMiddleware, deleteBasketTypeById);
+router.delete("/delete/:id", authMiddleware, isAdmin, deleteBasketTypeById);
 
 // SOFT DELETE a basket type by ID
-router.delete("/remove/:id", authMiddleware, softDeleteBasketTypeById);
+router.delete("/remove/:id", authMiddleware, isAdmin, softDeleteBasketTypeById);
 
 export default router;
