18b51c62cb184c49fc26d6ce5d4ce9fc84b5a969
milan <milanjulinac996@gmail.com>
2023-03-15 15:17:44 +0100

    done for auth

--- src/conrollers/auth.controller.ts
+++ src/conrollers/auth.controller.ts
@@ -1,92 +1,58 @@
-import { Request, Response, NextFunction } from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import User, { IUserModel } from '../models/User.model';
+import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User, { IUserModel } from "../models/User.model";
+import AuthService from "../services/auth.service";
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "";
 
 // Register a new user
 export const registerUser = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { firstName, lastName, email, password, confirmPassword } = req.body;
+	try {
+		const { firstName, lastName, email, password, confirmPassword } = req.body;
 
-    // Check if user already exists
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      res.status(400).json({ message: 'User already exists' });
-      return;
-    }
+		const result = await AuthService.registerUser(firstName, lastName, email, password, confirmPassword);
 
-    // Check if passwords match
-    if (password !== confirmPassword) {
-      res.status(400).json({ message: 'Passwords do not match' });
-      return;
-    }
-
-    // Hash password
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    // Create new user
-    const newUser: IUserModel = new User({
-      firstName,
-      lastName,
-      email,
-      passwordHash: hashedPassword,
-      deleted: false,
-      role: 'customer'
-    });
-    const savedUser = await newUser.save();
-
-    // Create and sign JWT
-    const token = jwt.sign({ id: savedUser._id, role: savedUser.role, email: savedUser.email }, JWT_SECRET_KEY);
-
-    // Send response
-    res.status(201).json({ token });
-  } catch (error) {
-    res.status(500).json({ message: 'Error registering user', error });
-  }
+		if (result) {
+			res.status(201).json({ token: result.token });
+		} else {
+			res.status(400).json({ message: "User already exists or passwords do not match" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "Error registering user", error });
+	}
 };
 
 // Log in an existing user
 export const loginUser = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { email, password } = req.body;
+	try {
+		const { email, password } = req.body;
 
-    // Check if user exists
-    const user = await User.findOne({ email });
-    if (!user) {
-      res.status(401).json({ message: 'Invalid email or password' });
-      return;
-    }
+		const result = await AuthService.loginUser(email, password);
 
-    // Check password
-    const isMatch = await bcrypt.compare(password, user.passwordHash);
-    if (!isMatch) {
-      res.status(401).json({ message: 'Invalid email or password' });
-      return;
-    }
-
-    // Create and sign JWT
-    const token = jwt.sign({ id: user._id, role: user.role, email: user.email }, JWT_SECRET_KEY, { expiresIn: '1h' });
-
-    // Set JWT as a cookie
-    res.cookie('jwt', token, { httpOnly: true, secure: false, sameSite: 'none', maxAge: 60 * 60 * 1000 }); // Expires after 1 hour
-
-    // Send response
-    res.status(200).json({ message: "User logged in successfully", token: token });
-  } catch (error) {
-    res.status(500).json({ message: 'Error logging in user', error });
-  }
+		if (result) {
+			res.cookie("jwt", result.token, {
+				httpOnly: true,
+				secure: false,
+				sameSite: "none",
+				maxAge: 60 * 60 * 1000
+			}); // Expires after 1 hour
+			res.status(200).json({ message: "User logged in successfully", token: result.token });
+		} else {
+			res.status(401).json({ message: "Invalid email or password" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "Error logging in user", error });
+	}
 };
 
 // Logout user
 export const logoutUser = async (req: Request, res: Response): Promise<void> => {
-  try {
-    res.clearCookie('jwt');
+	try {
+		res.clearCookie("jwt");
 
-    res.status(200).json({ message: "Logout successful" });
-  } catch (error) {
-    res.status(500).json({ message: "Error logging out", error });
-  }
+		res.status(200).json({ message: "Logout successful" });
+	} catch (error) {
+		res.status(500).json({ message: "Error logging out", error });
+	}
 };
---
+++ src/services/auth.service.ts
@@ -0,0 +1,69 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User, { IUserModel } from "../models/User.model";
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "";
+
+class AuthService {
+	public async registerUser(
+		firstName: string,
+		lastName: string,
+		email: string,
+		password: string,
+		confirmPassword: string
+	): Promise<{ token: string } | null> {
+		// Check if user already exists
+		const userExists = await User.findOne({ email });
+		if (userExists) {
+			return null;
+		}
+
+		// Check if passwords match
+		if (password !== confirmPassword) {
+			return null;
+		}
+
+		// Hash password
+		const saltRounds = 10;
+		const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+		// Create new user
+		const newUser: IUserModel = new User({
+			firstName,
+			lastName,
+			email,
+			passwordHash: hashedPassword,
+			deleted: false,
+			role: "customer"
+		});
+		const savedUser = await newUser.save();
+
+		// Create and sign JWT
+		const token = jwt.sign({ id: savedUser._id, role: savedUser.role, email: savedUser.email }, JWT_SECRET_KEY);
+
+		return { token };
+	}
+
+	public async loginUser(email: string, password: string): Promise<{ token: string } | null> {
+		// Check if user exists
+		const user = await User.findOne({ email });
+		if (!user) {
+			return null;
+		}
+
+		// Check password
+		const isMatch = await bcrypt.compare(password, user.passwordHash);
+		if (!isMatch) {
+			return null;
+		}
+
+		// Create and sign JWT
+		const token = jwt.sign({ id: user._id, role: user.role, email: user.email }, JWT_SECRET_KEY, {
+			expiresIn: "1h"
+		});
+
+		return { token };
+	}
+}
+
+export default new AuthService();
