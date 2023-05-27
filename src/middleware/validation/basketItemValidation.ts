9a7fa6ed928c2f890726b1ee67bc9e1cf2f273d6
milan <milanjulinac996@gmail.com>
2023-05-27 12:42:01 +0200

    added basket item validation middleware

---
+++ src/middleware/validation/basketItemValidation.ts
@@ -0,0 +1,23 @@
+import Joi from "joi";
+import { Request, Response, NextFunction } from "express";
+
+const createBasketItemValidation = (req: Request, res: Response, next: NextFunction) => {
+	const basketItemSchema = Joi.object({
+		name: Joi.string().required(),
+		description: Joi.string().required(),
+		price: Joi.number().required(),
+		weight: Joi.number().required(),
+		giftBasket: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)),
+		isSerbian: Joi.boolean().required().default(false),
+		deleted: Joi.boolean().required().default(false)
+	});
+
+	const { error } = basketItemSchema.validate(req.body);
+	if (error) {
+		return res.status(400).json({ error: error.details[0].message });
+	}
+
+	next();
+};
+
+export default createBasketItemValidation;
