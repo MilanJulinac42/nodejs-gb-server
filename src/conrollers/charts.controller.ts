bddbff1894580395244cdf7d01211346cdab6b48
milan <milanjulinac996@gmail.com>
2023-06-27 11:28:21 +0200

    gb-54 added date range for charts

--- src/conrollers/charts.controller.ts
+++ src/conrollers/charts.controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
 import ChartsService from "../services/charts.service";
+import { format, parse } from "date-fns";
 
 export const getSalesStatistics = async (req: Request, res: Response) => {
 	try {
-		const salesStatistics = await ChartsService.getSalesStatistics();
+		const inputDateFormat = "dd-MM-yyyy";
+		const startDateInput = (req.query.startDate as string) || "01-01-1970";
+		const endDateInput = (req.query.endDate as string) || format(new Date(), inputDateFormat);
+
+		const startDate = format(parse(startDateInput, inputDateFormat, new Date()), "yyyy-MM-dd");
+		const endDate = format(parse(endDateInput, inputDateFormat, new Date()), "yyyy-MM-dd");
+
+		const salesStatistics = await ChartsService.getSalesStatistics(startDate, endDate);
 
 		res.json(salesStatistics);
 	} catch (error) {
--- src/services/charts.service.ts
+++ src/services/charts.service.ts
@@ -1,9 +1,17 @@
 import OrderModel from "../models/Order.model";
 
 class ChartsService {
-	public async getSalesStatistics() {
+	public async getSalesStatistics(startDate: string, endDate: string) {
 		try {
 			const salesStatistics = await OrderModel.aggregate([
+				{
+					$match: {
+						createdAt: {
+							$gte: new Date(new Date(startDate).setHours(0, 0, 0)),
+							$lt: new Date(new Date(endDate).setHours(23, 59, 59))
+						}
+					}
+				},
 				{
 					$group: {
 						_id: null,
