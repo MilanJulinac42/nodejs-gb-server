4feadd6c2286dd6074e4e3b7375ac04b82dbbbf1
MilanJulinac42 <milanjulinac996@gmail.com>
2023-10-09 23:14:56 +0200

    gb-85 added settings route

--- src/routes/settings.route.ts
+++ src/routes/settings.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import authMiddleware from "../middleware/JWT";
+import { isAdmin } from "../middleware/IsAdmin";
+import { createSettings, getSettings, updateSettings } from "../conrollers/settings.controller";
 
 const router: Router = Router();
 
 // CREATE settings
+router.post("/create", authMiddleware, isAdmin, createSettings);
 
 // READ settings
+router.get("/find", authMiddleware, isAdmin, getSettings);
 
 // UPDATE settings
+router.patch("/update-settings", authMiddleware, isAdmin, updateSettings);
